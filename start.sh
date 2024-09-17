f9cf27f42c4ab337da0f0b2ac08103055a7089ad
juger1 <gajehaj8@gmail.com>
2024-09-17 20:48:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/juger1/content /contact
+  git clone https://github.com/juger1/content /content
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
   git clone $SOURCE_CODE /URL-Shortener-V2
 fi
-cd /URL-Shortener-V2
+cd /content
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 get_config.py && python3 main.py
